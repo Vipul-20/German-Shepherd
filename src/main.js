b89df453e23ab6bfa41ae77ad5adb1cb11d08c3b
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-10-24 14:01:50 +0000

    capitalize the first letter & and console log each bread name

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ const selectEL = document.querySelector("select");
 /* API */
 const BASE_URL = `https://dog.ceo/api/`;
 
-
 // ==== getDogBread function ===
 function getDogBread() {
   return fetch(`${BASE_URL}breeds/list/all`)
@@ -13,18 +12,20 @@ function getDogBread() {
       // console.log(Object.keys(data.message));
       return Object.keys(data.message);
     })
-    .catch((error) => console.log(error));
+    .catch((error) => aleart(error));
 }
 getDogBread();
-
 
 // ==== renderOption function ====
 function renderOption() {
   getDogBread().then((data) => {
     const fragment = document.createDocumentFragment();
     for (let bread of data) {
+      // console.log(bread);(log each name of bread)
       const option = document.createElement("option");
-      option.textContent = bread;
+      /*create a new element in HTML,this method to create a DOM Node */
+      option.textContent = bread.charAt(0).toUpperCase() + bread.slice(1);
+      /*charAt(0)-it extract the first charater of string,slice(1)-slice the string starting from the second charter (index 1) to end */
       fragment.appendChild(option);
     }
     selectEL.appendChild(fragment);
